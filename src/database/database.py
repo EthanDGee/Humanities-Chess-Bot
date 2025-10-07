67ad9ba08af186bf14b30b10ef4c89ef69dbaf7b
EthanDGee <workethangee@gmail.com>
2025-10-07 16:24:49 -0600

    Add function to ensure database tables exist
    - Implemented `_ensure_table_exist` method to create `games` table if not present.
    - Added helper method `_append_columns` to streamline column definitions for INTEGER and TEXT types.

--- src/database/database.py
+++ src/database/database.py
@@ -2,13 +2,14 @@ import sqlite3
 from typing import List
 import pandas as pd
 
+
 class Database:
     def __init__(self, db_name: str):
         self.db_name = db_name
 
         # HEADER INFO
-        self.INTEGER_LABELS : List[str] = ["white_elo", "black_elo"]
-        self.BOOLEAN_LABELS : List[str] = ["is_black"]
+        self.INTEGER_LABELS: List[str] = ["white_elo", "black_elo"]
+        self.BOOLEAN_LABELS: List[str] = ["is_black"]
         # Columns exactly as in the provided selection
         self.CLASS_LABELS: List[str] = [
             # board squares
@@ -22,4 +23,29 @@ class Database:
             "H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8",
         ]
         self.TARGET_COLUMN: str = "selected_move"
+
+    def _ensure_table_exist(self) -> None:
+        def _append_columns(headers : List[str], data_type: str) -> str:
+
+            new_columns = ""
+            for header in headers:
+                new_columns += f"{header} {data_type},"
+
+            return new_columns
+
+        """Create tables if they don't exist"""
+        with sqlite3.connect(self.db_name) as conn:
+            cur = conn.cursor()
+
+            query = "CREATE TABLE IF NOT EXISTS games ("
+
+            # add the various labels
+            query += _append_columns(self.INTEGER_LABELS, "INTEGER")
+            query += _append_columns(self.BOOLEAN_LABELS, "INTEGER")
+            query += _append_columns(self.CLASS_LABELS, "INTEGER") # classes are stored as indexes
+
+            # add the target class
+            query += f"{self.TARGET_COLUMN} TEXT)"
+            cur.execute(query)
+            conn.commit()
 
